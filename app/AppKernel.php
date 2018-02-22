d26b17aa1e7fc046136eabc9e0d61fa15d17b6b6
ron-a-dal@hotmail.fr <Reyeldog971>
2018-02-22 11:13:14 +0100

    src

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,13 +16,15 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SS\PlatformBundle\SSPlatformBundle(),
+            new SS\UserBundle\SSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
+			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+			
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
